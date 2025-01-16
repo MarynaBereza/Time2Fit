5d3fd0972eb74735eddde52e281ec74a0b97ce08
MarynaLytvynenko <bereza.maryna92@gmail.com>
2025-01-16 13:57:34 +0200

    Initial Commit

---
+++ MLTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLTimer
+//
+//  Created by Maryna Bereza on 16.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
